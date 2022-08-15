f3864c5fbdc53c7aa87e1392461d464fce271bb2
Jean-LC <jeanettelc@outlook.com>
2022-08-15 10:46:28 -0500

    división de código en módulos

--- src/data.js
+++ src/data.js
@@ -1,7 +1,31 @@
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  //mostrar nombre de personajes con su imagen
+export const showData = (films, requiredData) => {
+  let allTheData = [];
+  films.forEach(item => allTheData.push (item[requiredData].filter((requiredItem) => requiredItem.name)))
+  return allTheData.flat ()
+}
+
+/*
+//mostrar nombre de vehículos con su imagen
+  export const showVehicles = (films) => {
+    const items = films.map((item) => {
+      return item.vehicles.map ((vehicles) => {
+        return [vehicles.name, vehicles.img]
+      })
+    })
+    return items
+  }
+
+//mostrar nombre de locaciones con su imagen
+export const showLocations = (films) => {
+  const items = films.map((item) => {
+    return item.locations.map((locations) => {
+      return [locations.name, locations.img]
+    })
+  })
+  return items
+}
+*/
+
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { showData } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 const textNav = document.getElementById("textNav");
@@ -8,7 +9,9 @@ const btnCharacters = document.getElementById("btnCharacters");
 const btnVehicles = document.getElementById("btnVehicles");
 const btnLocations = document.getElementById("btnLocations");
 const btnShowAll = document.getElementById("btnShowAll");
+//const btnOrderAZ = document.getElementById("btnOrderAZ");*/
 
+// SIDE NAV
 btnNav.addEventListener("click", function () {
     textNav.style.display = "block";
 });
@@ -17,107 +20,76 @@ btnClose.addEventListener("click", function () {
     textNav.style.display = "none";
 });
 
-let firstDiv = document.getElementById("cloneSection");
+console.log(Object.keys(data.films[0].people).length)
 
+
+//Cloner
+let firstDiv = document.getElementById("cloneSection");
 let cloner = (filmData) => {
     let secondDiv = firstDiv.cloneNode(true);
-    secondDiv.children[1].src = filmData.img;
-    secondDiv.children[0].innerHTML = filmData.name;
     secondDiv.id = filmData.id;
+    secondDiv.children[1].src = filmData.img;
+    secondDiv.children[0].innerText = filmData.name;
     secondDiv.addEventListener('click', function () { dataWindow(secondDiv.id); });
     document.getElementById('bigDivs').appendChild(secondDiv);
     secondDiv.style.display = 'block';
 }
 
-for (let x = 0; x < data.films.length; x++) {
-    cloner(data.films[x]);
-    data.films[x].people.forEach(character => {
-    cloner(character);
-    });
-    data.films[x].vehicles.forEach(vehicle => {
-    cloner(vehicle);
-    });
-    data.films[x].locations.forEach(location => {
-    cloner(location);
-    });
-    }
+//BOTONES PARA FILTRADO
+btnMovies.addEventListener("click", function () {
+    let allData = document.getElementsByClassName("sectionToClone");
+    while (allData.length > 0) allData[0].remove();
 
-
-btnMovies.addEventListener("click", function() {
-    const removeSection = document.getElementById("bigDivs")
-    removeSection.parentNode.removeChild(removeSection)
-
-    const newDiv = document.createElement("div");
-    newDiv.id="bigDivs"
-    document.body.appendChild(newDiv)
-
-    for (let x = 0; x < data.films.length; x++) {
-    cloner(data.films[x]);}
+    data.films.forEach(cloner)
 })
 
 
-btnCharacters.addEventListener("click", function() {
+btnCharacters.addEventListener("click", () => {
+    let allData = document.getElementsByClassName("sectionToClone");
+    while (allData.length > 0) allData[0].remove();
 
-    const removeSection = document.getElementById("bigDivs")
-    removeSection.parentNode.removeChild(removeSection)
+    showData(data.films, "people").forEach(cloner)
+})
 
-    const newDiv = document.createElement("div");
-    newDiv.id="bigDivs"
+btnVehicles.addEventListener("click", () => {
+    let allData = document.getElementsByClassName("sectionToClone");
+    while (allData.length > 0) allData[0].remove();
 
-    document.body.appendChild(newDiv)
-    firstDiv.remove();
+    showData(data.films, "vehicles").forEach(cloner)
+})
+
+btnLocations.addEventListener("click", () => {
+    let allData = document.getElementsByClassName("sectionToClone");
+    while (allData.length > 0) allData[0].remove();
+
+    showData(data.films, "locations").forEach(cloner)
+})
+
+btnShowAll.addEventListener("click", () => {
+    let allData = document.getElementsByClassName("sectionToClone");
+    while (allData.length > 0) allData[0].remove();
+
+    data.films.forEach(cloner)
+    showData(data.films, "people").forEach(cloner)
+    showData(data.films, "vehicles").forEach(cloner)
+    showData(data.films, "locations").forEach(cloner)
+})
+
+//MOSTRAR TODO AL INICIO DE LA PÁGINA
+data.films.forEach(cloner);
+showData(data.films, "people").forEach(cloner);
+showData(data.films, "vehicles").forEach(cloner);
+showData(data.films, "locations").forEach(cloner);
+
+// information function
+const showData2 = document.getElementById("root");
+showData2.addEventListener('click', function () { showData2.style.display = 'none' })
+function dataWindow(filmID) {
+    showData2.style.display = "block"
+
+
     for (let x = 0; x < data.films.length; x++) {
-        data.films[x].people.forEach(character => cloner(character))}
-    })
-
-btnVehicles.addEventListener("click", function () {
-        let allData = document.getElementsByClassName("sectionToClone");
-        while (allData.length > 0) allData[0].remove();
-        for (let x = 0; x < data.films.length; x++) {
-            data.films[x].vehicles.forEach(vehicles => {
-                cloner(vehicles);
-            })
-        }
-    })
-    
-    btnLocations.addEventListener("click", function () {
-        let allData = document.getElementsByClassName("sectionToClone");
-        while (allData.length > 0) allData[0].remove();
-        for (let x = 0; x < data.films.length; x++) {
-            data.films[x].locations.forEach(location => {
-                cloner(location);
-            })
-        }
-    })
-    
-    btnShowAll.addEventListener("click", function(){
-        const removeSection = document.getElementById("bigDivs")
-        removeSection.parentNode.removeChild(removeSection)
-    
-        const newDiv = document.createElement("div");
-        newDiv.id="bigDivs"
-        document.body.appendChild(newDiv)
-        
-        for (let x = 0; x < data.films.length; x++) {
-            cloner(data.films[x]);
-            data.films[x].people.forEach(character => {
-            cloner(character);
-            });
-            data.films[x].vehicles.forEach(vehicle => {
-            cloner(vehicle);
-            });
-            data.films[x].locations.forEach(location => {
-            cloner(location);
-            });
-            }        
-    })
-
-const showData = document.getElementById("root");
-showData.addEventListener('click', function () { showData.style.display = 'none' })
-function dataWindow(filmName) {
-    showData.style.display = "block"
-    for (let x = 0; x < data.films.length; x++) {
-        if (filmName === data.films[x].id) {
+        if (filmID === data.films[x].id) {
             document.getElementById('data1').innerHTML = ('Name: ' + data.films[x].name);
             document.getElementById('data2').innerHTML = ('Description: ' + data.films[x].description);
             document.getElementById('data3').innerHTML = ('Director: ' + data.films[x].director);
@@ -128,35 +100,43 @@ function dataWindow(filmName) {
         }
         else {
             data.films[x].people.forEach(name => {
-                if (filmName === name.id) {
-                document.getElementById('data1').innerHTML = ('Name: ' + name.name);
-                document.getElementById('data2').innerHTML = ('Age: ' + name.age);
-                document.getElementById('data3').innerHTML = ('Eye color: ' + name.eye_color);
-                document.getElementById('data4').innerHTML = ('Gender: ' + name.gender);
-                document.getElementById('data5').innerHTML = ('Hair color: ' + name.hair_color);
-                document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].name);
+                if (filmID === name.id) {
+                    document.getElementById('data1').innerHTML = ('Name: ' + name.name);
+                    document.getElementById('data2').innerHTML = ('Age: ' + name.age);
+                    document.getElementById('data3').innerHTML = ('Eye color: ' + name.eye_color);
+                    document.getElementById('data4').innerHTML = ('Gender: ' + name.gender);
+                    document.getElementById('data5').innerHTML = ('Hair color: ' + name.hair_color);
+                    document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].name);
                 }
-                });
-                data.films[x].vehicles.forEach(name => {
-                if (filmName === name.id) {
-                document.getElementById('data1').innerHTML = ('Name: ' + name.name);
-                document.getElementById('data2').innerHTML = ('Description: ' + name.description);
-                document.getElementById('data3').innerHTML = ('Vehicle class: ' + name.vehicle_class);
-                document.getElementById('data4').innerHTML = ('Pilot: ' + name.pilot.name);
-                document.getElementById('data5').innerHTML = ('Length: ' + name.length);
-                document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].name);
+            });
+            data.films[x].vehicles.forEach(name => {
+                if (filmID === name.id) {
+                    document.getElementById('data1').innerHTML = ('Name: ' + name.name);
+                    document.getElementById('data2').innerHTML = ('Description: ' + name.description);
+                    document.getElementById('data3').innerHTML = ('Vehicle class: ' + name.vehicle_class);
+                    document.getElementById('data4').innerHTML = ('Pilot: ' + name.pilot.name);
+                    document.getElementById('data5').innerHTML = ('Length: ' + name.length);
+                    document.getElementById('data6').innerHTML = ('Apears in: ' + data.films[x].name);
                 }
-                });
-                data.films[x].locations.forEach(name => {
-                if (filmName === name.id) {
-                document.getElementById('data1').innerHTML = ('Name: ' + name.name);
-                document.getElementById('data2').innerHTML = ('Climate: ' + name.climate);
-                document.getElementById('data3').innerHTML = ('Residents: ' + name.residents);
-                document.getElementById('data4').innerHTML = ('Water surface: ' + name.surface_water);
-                document.getElementById('data5').innerHTML = ('Terrain: ' + name.terrain);
-                document.getElementById('data6').innerHTML = ('Appears in: ' + data.films[x].name);
+            });
+            data.films[x].locations.forEach(name => {
+                if (filmID === name.id) {
+                    document.getElementById('data1').innerHTML = ('Name: ' + name.name);
+                    document.getElementById('data2').innerHTML = ('Climate: ' + name.climate);
+                    document.getElementById('data3').innerHTML = ('Residents: ' + name.residents);
+                    document.getElementById('data4').innerHTML = ('Water surface: ' + name.surface_water);
+                    document.getElementById('data5').innerHTML = ('Terrain: ' + name.terrain);
+                    document.getElementById('data6').innerHTML = ('Appears in: ' + data.films[x].name);
                 }
-                });
+            });
         }
     }
 }
+
+
+
+
+
+
+
+
