71c96c6b969a074f56cbe22f925a3b97ab53144d
Jean-LC <jeanettelc@outlook.com>
2022-08-23 10:28:56 -0500

    add test expect not to be

--- test/data.spec.js
+++ test/data.spec.js
@@ -18,6 +18,15 @@ describe('show required Data', () => {
   it('returns `locations`', () => {
     expect(showData(tryArrayforOther.films, "locations")).toStrictEqual([{ "name": "londron", "climate": "foggy" }]);
   });
+  it('does not return `noname` people ', () => {
+    expect(showData(tryArrayforOther.films, "people")).not.toBe([{"noname": "Lulei", "age": "17"}]);
+  });
+  it('does not return `noname` vehicles ', () => {
+    expect(showData(tryArrayforOther.films, "vehicles")).not.toBe([{"noname": "godKiller", "vehicleStats": "over9thousand"}]);
+  });
+  it('does not return `noname` locations ', () => {
+    expect(showData(tryArrayforOther.films, "locations")).not.toBe([{"noname": "pagrizzzz", "climate": "Cold"}]);
+  });
 });
 
 
@@ -29,6 +38,9 @@ describe('order A-Z', () => {
   it('returns `order`', () => {
     expect(orderAZ(tryArrayforOrder.films)).toStrictEqual([{ "name": "Abigail" }, { "name": "Zandra" }]);
   });
+  it('does not return `order` from Z-A ', () => {
+    expect(orderAZ(tryArrayforOrder.films)).not.toBe([{ "name": "Zandra" }, { "name": "Abigail" }]);
+  });
 });
 
 //PROBAR FUNCIÓN DE ORDENAR Z-A
@@ -38,5 +50,8 @@ describe('order Z-A', () => {
   });
   it('returns `order`', () => {
     expect(orderZA(tryArrayforOrder.films)).toStrictEqual([{ "name": "Zandra" }, { "name": "Abigail" }]);
+  });
+  it('does not return `order` from A-Z ', () => {
+    expect(orderZA(tryArrayforOrder.films)).not.toBe([{ "name": "Abigail" }, { "name": "Zandra" }]);
   });
 });
